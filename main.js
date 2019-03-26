bef2f62f5b76c01916824f0eeeeec4bc7d7db97f
tseti <fred_luo@msn.com>
2019-03-26 21:21:20 +0100

    Add optional intensity and color logic and its associated html controls

--- main.js
+++ main.js
@@ -3,14 +3,22 @@
 const html      = document.getElementsByTagName('html')[0];
 const container = document.querySelector('.container');
 const clearBtn  = document.querySelector('.clear');
+const form      = document.querySelector('form');
+const radioBtns = document.getElementsByTagName('input');
+
+const BLACK  = 0;
+const RANDOM = 1;
 
 let sketchpad = document.querySelector('.sketchpad');
-let gridSize = 32;
+let gridSize  = 16;
+let intensity = 1;
+let color     = BLACK;
 
 clearBtn.addEventListener('click', () => changeSize(prompt('Enter new size', 64)));
 
-
+form.reset();
 addDivsToContainer();
+addRadioBtnListeners();
 
 function changeSize(size) {
   gridSize = size;
@@ -23,17 +31,56 @@ function changeSize(size) {
 
 
 function addDivsToContainer() {
-  for (let i = 0; i < gridSize**2; i++) {
+  for (let i = 0; i < gridSize ** 2; i++) {
     let div = document.createElement('div');
     div.classList.add(`cell${i}`);
-    div.classList.add('color-cell');
     addListener(div);
-
     sketchpad.appendChild(div);
     html.style.setProperty("--side-number", gridSize);
   }
 }
 
 function addListener(elem) {
-  elem.addEventListener('mouseover', (e) => e.target.classList.add('black-bg'));
+  elem.addEventListener('mouseover', function(e) {
+    let currentColor = e.target.style['background-color'];
+
+    if (currentColor) {
+      e.target.style['background-color'] = updateIntensity(currentColor);
+    } else {
+      e.target.style['background-color'] = color === BLACK ?
+        `rgba(0, 0, 0, ${intensity}` : getRandomColor();
+    }
+  });
+}
+
+function updateIntensity(color) {
+  const hasMaxIntensity = color.slice(0, 4) === 'rgb(';
+  if (!hasMaxIntensity) {
+    const newIntensity = +color.slice(-4, -1) + intensity;
+     return `${color.slice(0, -4)} ${newIntensity})`;
+  }
+  return color;
+
+}
+
+function getRandomColor() {
+  return `rgba(${getRandom256()}, ${getRandom256()}, ${getRandom256()}
+      , ${intensity})`;
+}
+
+function getRandom256() {
+  return Math.trunc(Math.random() * 256);
+}
+
+function addRadioBtnListeners() {
+  Array.from(radioBtns).forEach(radioBtn => {
+    radioBtn.addEventListener('click', function(e) {
+      if (e.target.name === 'intensity') {
+        intensity = +e.target.value;
+      } else {
+        color = +e.target.value;
+      }
+      changeSize(gridSize);
+    });
+  });
 }
