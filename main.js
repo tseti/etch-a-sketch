7935b8eacb892b4da4286666de7d0a0a2b1210e1
tseti <fred_luo@msn.com>
2019-03-26 19:03:11 +0100

    Add sketch grid creation and styling

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 "use strict"
+
+const html      = document.getElementsByTagName('html')[0];
+const container = document.querySelector('.container');
+const clearBtn  = document.querySelector('.clear');
+
+let sketchpad = document.querySelector('.sketchpad');
+let gridSize = 32;
+
+clearBtn.addEventListener('click', () => changeSize(prompt('Enter new size', 64)));
+
+
+addDivsToContainer();
+
+function addDivsToContainer() {
+  for (let i = 0; i < gridSize**2; i++) {
+    let div = document.createElement('div');
+    div.classList.add(`cell${i}`);
+    div.classList.add('color-cell');
+    sketchpad.appendChild(div);
+    html.style.setProperty("--side-number", gridSize);
+  }
+}
+
+function changeSize(size) {
+  gridSize = size;
+  container.removeChild(sketchpad);
+  sketchpad = document.createElement('div');
+  sketchpad.classList.add('sketchpad');
+  container.appendChild(sketchpad);
+  addDivsToContainer();
+}
